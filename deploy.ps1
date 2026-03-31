464f992227de10fb8b820aca61badcf2487950df
chaimae <maakoulchaimae4@gmail.com>
2026-03-31 10:05:45 +0100

    commit de tp5 docker devOps

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
 # Arrêter et supprimer l'ancien conteneur
-try { docker stop flask_app } catch {}
-try { docker rm flask_app } catch {}
+try { docker stop tp5_docker } catch {}
+try { docker rm tp5_docker } catch {}
 
 # Tirer la dernière image
 docker pull chaimae2006/tp5_docker:latest
 
 # Lancer le conteneur
-docker run -d --name flask_app -p 5000:5000 chaimae2006!tp5_docker:latest
+docker run -d --name tp5_docker -p 5000:5000 chaimae2006!tp5_docker:latest
